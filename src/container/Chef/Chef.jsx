d0917e2df87f867969b725a659433bb8a46f9ea1
Salvador Poeta <dsmlhidroponia@gmail.com>
2026-04-01 09:22:57 -0600

    fix img2

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { SubHeading } from '../../components'
 import { images } from '../../constants'
 import './Chef.css'
@@ -6,14 +7,7 @@ import './Chef.css'
 const Chef = () => (
   <div className="app__bg app__wrapper section__padding">
     <div className="app__wrapper_img app__wrapper_img-reverse">
-      {/* Añadimos width y height basados en el reporte de PageSpeed (emulado) */}
-      <img
-        src={images.chef}
-        alt="chef"
-        width="348"
-        height="433"
-        loading="lazy"
-      />
+      <img src={images.chef} alt="chef" />
     </div>
 
     <div className="app__wrapper_info">
@@ -22,8 +16,7 @@ const Chef = () => (
 
       <div className="app__chef-content">
         <div className="app__chef-content_quote">
-          {/* Imagen pequeña, pero igual con dimensiones para evitar saltos */}
-          <img src={images.quote} alt="quote" width="40" height="40" />
+          <img src={images.quote} alt="quote" />
           <p className="p__opensans">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit.
           </p>
@@ -38,52 +31,10 @@ const Chef = () => (
       <div className="app__chef-sign">
         <p>Kevin Luo</p>
         <p className="p__opensans">Chef & Founder</p>
-        {/* Esta es la firma que Lighthouse marcaba como PNG pesado */}
-        <img src={images.sign} alt="sign" width="278" height="108" />
+        <img src={images.sign} alt="sign" />
       </div>
     </div>
   </div>
 )
 
 export default Chef
-
-// import React from 'react'
-
-// import { SubHeading } from '../../components'
-// import { images } from '../../constants'
-// import './Chef.css'
-
-// const Chef = () => (
-//   <div className="app__bg app__wrapper section__padding">
-//     <div className="app__wrapper_img app__wrapper_img-reverse">
-//       <img src={images.chef} alt="chef" />
-//     </div>
-
-//     <div className="app__wrapper_info">
-//       <SubHeading title="Chef's Word" />
-//       <h1 className="headtext__cormorant">What we believe in</h1>
-
-//       <div className="app__chef-content">
-//         <div className="app__chef-content_quote">
-//           <img src={images.quote} alt="quote" />
-//           <p className="p__opensans">
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit.
-//           </p>
-//         </div>
-//         <p className="p__opensans">
-//           auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit.
-//           Nulla scelerisque scelerisque congue ac consequat, aliquam molestie
-//           lectus eu. Congue iaculis integer curabitur semper sit nunc.
-//         </p>
-//       </div>
-
-//       <div className="app__chef-sign">
-//         <p>Kevin Luo</p>
-//         <p className="p__opensans">Chef & Founder</p>
-//         <img src={images.sign} alt="sign" />
-//       </div>
-//     </div>
-//   </div>
-// )
-
-// export default Chef
