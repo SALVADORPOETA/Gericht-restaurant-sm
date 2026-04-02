b77114dc65a3d21a263f42e071202ab1b25a2da4
Salvador Poeta <dsmlhidroponia@gmail.com>
2026-04-02 01:03:52 -0600

    fix img3

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,34 +1,74 @@
-import React from 'react';
+import React from 'react'
 
-import { images } from '../../constants';
-import './AboutUs.css';
+import { images } from '../../constants'
+import './AboutUs.css'
 
 const AboutUs = () => (
-  <div className='app__aboutus app__bg flex__center section__padding' id='about' >
-    <div className='app__aboutus-overlay flex__center' >
-      <img src={images.G} alt='g letter' />
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img
+        src={images.G}
+        alt="g letter"
+        width="391"
+        height="415"
+        loading="lazy"
+      />
     </div>
-    <div className='app__aboutus-content flex__center' >
-      <div className='app__aboutus-content_about' >
-        <h1 className='headtext__cormorant' >About Us</h1>
-        <img src={images.spoon} alt='about_spoon' className='spoon__img' />
-        <p className='p__opensans' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-        <button type='button' className='custom__button' >Know More</button>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img
+          src={images.spoon}
+          alt="about_spoon"
+          className="spoon__img"
+          width="45"
+          height="15"
+          loading="lazy"
+        />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
       </div>
 
-      <div className='app__aboutus-content_knife flex__center' >
-        <img src={images.knife} alt='about_knife' />
+      <div className="app__aboutus-content_knife flex__center">
+        <img
+          src={images.knife}
+          alt="about_knife"
+          width="133"
+          height="1110"
+          loading="lazy"
+        />
       </div>
 
-      <div className='app__aboutus-content_history' >
-        <h1 className='headtext__cormorant' >Our History</h1>
-        <img src={images.spoon} alt='about_spoon' className='spoon__img' />
-        <p className='p__opensans' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-        <button type='button' className='custom__button' >Know More</button>
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img
+          src={images.spoon}
+          alt="about_spoon"
+          className="spoon__img"
+          width="45"
+          height="15"
+          loading="lazy"
+        />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
       </div>
-
     </div>
   </div>
-);
+)
 
-export default AboutUs;
+export default AboutUs
